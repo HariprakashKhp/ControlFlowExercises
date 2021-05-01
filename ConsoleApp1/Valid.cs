9344718e02ff8e4b4c27c37a6c743b734f22afb1
HariPrakash <hariprakash.khp@gmail.com>
2021-05-01 20:40:09 +0530

    Add project files.

---
+++ ConsoleApp1/Valid.cs
@@ -0,0 +1,21 @@
+﻿//Question 1 
+
+using System;
+namespace ConsoleApp1
+{
+    public class Valid
+    {
+        string stringRead;
+        public void ReadStream()
+        {
+            Console.WriteLine("Please enter a number from 1 to 10");
+            stringRead =  Console.ReadLine();
+        }
+
+        public void writeStream()
+        {
+            int number = Convert.ToInt32(stringRead);
+            Console.WriteLine(number < 10 && number > 0 ? "Valid": "Invalid");
+        }
+    }
+}
